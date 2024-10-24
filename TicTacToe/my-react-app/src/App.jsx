5b2ef37deb9e80578c0e6afa0a9cfde57aa7b52f
Thai Anh Hoang <bohous98@gmail.com>
2024-10-24 22:35:13 +0200

    TicTacToe: Putting marks into square

--- TicTacToe/my-react-app/src/App.jsx
+++ TicTacToe/my-react-app/src/App.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 import Tile from "./Tile.jsx";
 
 function App() {
-  
+
+  const [turn, setTurn] = useState(1)
+
+  //board stuff
   let dimension = 3
 
-  const board = [];
+  const board = []
 
   for (let i = 0; i < dimension; i++){
     const row = []
     for(let j = 0; j < dimension; j++){
+      const [value, setValue] = useState(i + ":" + j)
       row.push( 
-        <Tile key={j} x={i} y={j}/>
+        <Tile key={j} value={value} handleClick={() => handleClick(setValue)}/>
       )
     }
     board.push(<div className="d-flex" key={i}>{row}</div>)
   }
+
+  //--------------------------
+
+  const handleClick = (setValue)=>{    
+
+    if(turn % 2 === 0) setValue("X")
+    else setValue("O")
+
+    setTurn(t =>  t + 1)
+    console.log("turn: " + turn)
+}
+
 
   return (
     <div>
--- TicTacToe/my-react-app/src/Tile.jsx
+++ TicTacToe/my-react-app/src/Tile.jsx
@@ -1,6 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
-function Tile({x,y}){
+function Tile({value, handleClick}){
     const myStyle = {
         width: "200px",
         height: "200px"
@@ -8,8 +8,8 @@ function Tile({x,y}){
 
 
     return(
-        <div className="d-flex justify-content-center align-items-center border" style={myStyle}>
-            <h1 className="text-center opacity-25">{x} : {y}</h1>
+        <div className="d-flex justify-content-center align-items-center border" style={myStyle} onClick={handleClick}>
+            <h1 className="text-center">{value}</h1>
         </div>
     )
 }
